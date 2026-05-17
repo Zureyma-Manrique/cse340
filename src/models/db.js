50657bb999c9a08ecc55f3e4d9c9b8cf0e23a1ba
Zureyma-Manrique <quetzaili.manrique@gmail.com>
2026-05-17 00:51:13 -0600

    change db

--- src/models/db.js
+++ src/models/db.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 let db = null;
